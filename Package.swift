7a672861b120d25ba71d29c61e0ba967c4c7a000
Joshua Button <jbutton@bignerdranch.com>
2023-07-27 11:37:11 -0700

    Remove macOS from Package.swift

--- Package.swift
+++ Package.swift
@@ -6,9 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BottomSheet",
     platforms: [
-        .iOS(.v14),
-        .macCatalyst(.v13),
-        .macOS(.v10_15)
+        .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
